a18dee1733aea1f2aff19d726a949722f53c7b2b
obiwac <obiwac@gmail.com>
2021-12-05 10:17:08 +0100

    2021 day 5: literally in a single line

--- 2021/5/main.py
+++ 2021/5/main.py
@@ -1,4 +1,1 @@
-from collections import Counter
-from itertools import chain
-
-print(sum([x>1for x in Counter(chain(*[(lambda x,y,z,w:(lambda r,*a:r(r,*a))(lambda r,c,x,y,z,w,a,b:r(r,c+[(x,y)],x+a,y+b,z,w,a,b)if x!=z or y!=w else[(x,y)]+c,[],x,y,z,w,(z>x)-(z<x),(w>y)-(w<y)))(*chain(*l))for l in[[[int(z.strip())for z in y.split(',')]for y in x.split('->')]for x in open(0).readlines()]])).values()]))
+print((lambda n:sum([x>1for x in __import__("collections").Counter(n(*[(lambda x,y,z,w:(lambda r,*a:r(r,*a))(lambda r,c,x,y,z,w,a,b:r(r,c+[(x,y)],x+a,y+b,z,w,a,b)if x!=z or y!=w else[(x,y)]+c,[],x,y,z,w,(z>x)-(z<x),(w>y)-(w<y)))(*n(*l))for l in[[[int(z.strip())for z in y.split(',')]for y in x.split('->')]for x in open(0).readlines()]])).values()]))(__import__("itertools").chain))
