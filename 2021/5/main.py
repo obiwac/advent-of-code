841888b6c7dcda7ec8692aec04a50adcdd6c8874
obiwac <obiwac@gmail.com>
2021-12-05 09:54:23 +0100

    2021 day 5: getting there...

--- 2021/5/main.py
+++ 2021/5/main.py
@@ -1,22 +1,18 @@
 from itertools import accumulate
 from functools import reduce
+from itertools import chain
 
 f = [[[int(z.strip()) for z in y.split(',')] for y in x.split('->')] for x in map(str.strip, open(0).readlines())]
 # print(f)
 
 c = {}
 
-for line in f:
-	(x, y), (z, w) = line
+for l in f:
+	def r(c,x,y,z,w,a,b):
+		c[(x, y)] = c.get((x, y), 0) + 1
+		return r(c,x+a,y+b,z,w,a,b)if x!=z or y!=w else c
 
-	a = (z>x)-(z<x)
-	b = (w>y)-(w<y)
-
-	def recurse(c,x,y,z,w,a,b):
-		c[(x, y)] = c.get((x, y), 0) + 1 # not super functional
-		return recurse(c,x+a,y+b,z,w,a,b)if x!=z or y!=w else c
-
-	recurse(c, x, y, z, w, a, b)
+	(lambda x,y,z,w:r(c,x,y,z,w,(z>x)-(z<x),(w>y)-(w<y)))(*chain(*l))
 
 cnt = sum([x>1for x in c.values()])
 print(cnt)
