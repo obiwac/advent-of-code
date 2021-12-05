d70958bc273088368ab8c343565e9ee04cda4059
obiwac <obiwac@gmail.com>
2021-12-05 10:11:38 +0100

    2021 day 5: basically haskell

--- 2021/5/main.py
+++ 2021/5/main.py
@@ -1,18 +1,6 @@
-from itertools import accumulate
-from functools import reduce
+from collections import Counter
 from itertools import chain
 
 f = [[[int(z.strip()) for z in y.split(',')] for y in x.split('->')] for x in map(str.strip, open(0).readlines())]
-# print(f)
 
-c = {}
-
-for l in f:
-	def r(c,x,y,z,w,a,b):
-		c[(x, y)] = c.get((x, y), 0) + 1
-		return r(c,x+a,y+b,z,w,a,b)if x!=z or y!=w else c
-
-	(lambda x,y,z,w:r(c,x,y,z,w,(z>x)-(z<x),(w>y)-(w<y)))(*chain(*l))
-
-cnt = sum([x>1for x in c.values()])
-print(cnt)
+print(sum([x>1for x in Counter(chain(*[(lambda x,y,z,w:(lambda r,*a:r(r,*a))(lambda r,c,x,y,z,w,a,b:r(r,c+[(x,y)],x+a,y+b,z,w,a,b)if x!=z or y!=w else[(x,y)]+c,[],x,y,z,w,(z>x)-(z<x),(w>y)-(w<y)))(*chain(*l))for l in f])).values()]))
