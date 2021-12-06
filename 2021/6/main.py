ae8615f18816f87bf87c61de81cdc5ab58b6ee32
obiwac <obiwac@gmail.com>
2021-12-06 08:43:23 +0100

    2021 day 6: one-liner

--- 2021/6/main.py
+++ 2021/6/main.py
@@ -1,11 +1,3 @@
-from itertools import accumulate as accum
-from itertools import chain
-from functools import reduce
+f = open(0).read()
 
-f = [int(x) for x in map(str.strip, open(0).read().split(','))]
-f = [f.count(k)for k in range(10)]
-
-def r(r,d,f):
-	return r(r,d+1,[*map(lambda i:(f[i],f[i]+f[0],f[0])[1+(7,9).index(i)if i in(7,9)else 0],range(10))][1:]+[0])if d!=257else f
-
-print(sum([r(r,0,f)[x]for x in range(8)]))
+print(sum([(lambda r,*a:r(r,*a))(lambda r,d,f:r(r,d+1,[*map(lambda i:(f[i],f[i]+f[0],f[0])[1+(7,9).index(i)if i in(7,9)else 0],range(10))][1:]+[0])if d!=257else f,0,[[int(x)for x in map(str.strip,f.split(','))].count(k)for k in range(10)])[x]for x in range(8)]))
