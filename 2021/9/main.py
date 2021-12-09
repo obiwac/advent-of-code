47cf62bfe3a23e9c25a5bfc30b4de157e2057878
Obiwac <obiwac@gmail.com>
2021-12-09 11:46:13 +0100

    2021 day 9: getting thereeeee

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -1,28 +1,21 @@
-f = [x for x in map(str.strip, open(0).read().strip().split('\n'))]
+f = [[*map(int, x)] for x in map(str.strip, open(0).read().strip().split('\n'))]
 
 basins = []
+POSITIONS = lambda x, y: ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
 
 def recurse(f, d, x, y):
 	d.append((x, y))
-	spot = int(f[x][y])
 
 	def check(spot, d, f, x, y):
-		return recurse(f, d, x, y) if x in range(len(f)) and y in range(len(f[x])) and spot < int(f[x][y]) < 9 and (x, y) not in d else 0
+		return recurse(f, d, x, y) if x in range(len(f)) and y in range(len(f[x])) and spot < f[x][y] < 9 and (x, y) not in d else 0
 
-	return 1 + sum(map(lambda p: check(spot, d, f, *p), ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))))
+	return 1 + sum(map(lambda p: check(f[x][y], d, f, *p), POSITIONS(x, y)))
 
 for x in range(len(f)):
 	for y in range(len(f[x])):
-		spot = int(f[x][y])
+		adjacent = [f[z][w] for z, w in POSITIONS(x, y) if z in range(len(f)) and w in range(len(f[x]))]
 
-		adjacent = []
-
-		if x + 1 < len(f):    adjacent.append(int(f[x + 1][y]))
-		if x - 1 >= 0:        adjacent.append(int(f[x - 1][y]))
-		if y + 1 < len(f[x]): adjacent.append(int(f[x][y + 1]))
-		if y - 1 >= 0:        adjacent.append(int(f[x][y - 1]))
-
-		if any(spot < a for a in adjacent):
+		if any(f[x][y] < a for a in adjacent):
 			d = []
 			basins.append(recurse(f, d, x, y))
 
