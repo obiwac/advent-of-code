ccbb0907b8880993f97ae2468262c0ff3308a6a7
obiwac <obiwac@gmail.com>
2021-12-09 22:39:18 +0100

    2021 day 9: actually in one line

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -2,8 +2,8 @@ from functools import reduce
 from itertools import chain
 from operator import mul, or_
 
-f = [[*map(int, x)] for x in map(str.strip, open(0).read().strip().split('\n'))]
+i = open(0).read()
 
-o = (lambda p,q:reduce(mul,sorted(chain(*[[len((lambda r,*a:r(r,*a))(lambda r,f,x,y:{(x,y)}|reduce(or_,[({(x,y)}|r(r,f,z,w)if z in q(f)and w in q(f[x])and f[x][y]<f[z][w]<9else set())for z,w in p(x,y)]),f,x,y))for y in q(f[x])if any(f[x][y]<f[z][w]for z,w in p(x,y)if z in q(f)and w in q(f[x]))]for x in q(f)]))[-3:]))(lambda x,y:((x+1,y),(x-1,y),(x,y+1),(x,y-1)),lambda x:range(len(x)))
+o = (lambda p,q,f:reduce(mul,sorted(chain(*[[len((lambda r,*a:r(r,*a))(lambda r,f,x,y:{(x,y)}|reduce(or_,[({(x,y)}|r(r,f,z,w)if z in q(f)and w in q(f[x])and f[x][y]<f[z][w]<9else set())for z,w in p(x,y)]),f,x,y))for y in q(f[x])if any(f[x][y]<f[z][w]for z,w in p(x,y)if z in q(f)and w in q(f[x]))]for x in q(f)]))[-3:]))(lambda x,y:((x+1,y),(x-1,y),(x,y+1),(x,y-1)),lambda x:range(len(x)),[[*map(int,x)]for x in i.split('\n')])
 
 print(o)
