49636ab3b018aebeef24d826ede4b2a2ef26afb2
Obiwac <obiwac@gmail.com>
2021-12-09 19:44:58 +0100

    2021 day 9: just need to use that recursive lambda trick with `recurse` and I'm done

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -4,12 +4,11 @@ from functools import reduce
 POSITIONS = lambda x, y: ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
 
 def recurse(f, x, y):
-	def check(spot, f, x, y):
-		return {(x, y)} | recurse(f, x, y) if x in range(len(f)) and y in range(len(f[x])) and spot < f[x][y] < 9 else set()
-
-	return {(x, y)} | reduce(lambda a, b: a | b, map(lambda p: check(f[x][y], f, *p), POSITIONS(x, y)))
+	return {(x, y)} | reduce(lambda a, b: a | b, [({(x,y)}|recurse(f,z,w)if z in range(len(f))and w in range(len(f[x]))and f[x][y]<f[z][w]<9else set())for z,w in POSITIONS(x, y)])
 
 from itertools import chain
+from operator import mul
 
-basins = [*chain(*[[len(recurse(f, x, y)) for y in range(len(f[x])) if any(f[x][y] < f[z][w] for z, w in POSITIONS(x, y) if z in range(len(f)) and w in range(len(f[x])))] for x in range(len(f))])]
-print(reduce(lambda x, y: x * y, sorted(basins)[-3:]))
+o = reduce(mul,sorted(chain(*[[len(recurse(f, x, y)) for y in range(len(f[x])) if any(f[x][y] < f[z][w] for z, w in POSITIONS(x, y) if z in range(len(f)) and w in range(len(f[x])))] for x in range(len(f))]))[-3:])
+
+print(o)
