512a6eb9992a4562e770c8ce31a10398a65bf96d
obiwac <obiwac@gmail.com>
2021-12-09 22:32:19 +0100

    2021 day 9: one line

--- 2021/9/main.py
+++ 2021/9/main.py
@@ -1,14 +1,9 @@
-f = [[*map(int, x)] for x in map(str.strip, open(0).read().strip().split('\n'))]
 from functools import reduce
-
-POSITIONS = lambda x, y: ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-
-def recurse(f, x, y):
-	return {(x, y)} | reduce(lambda a, b: a | b, [({(x,y)}|recurse(f,z,w)if z in range(len(f))and w in range(len(f[x]))and f[x][y]<f[z][w]<9else set())for z,w in POSITIONS(x, y)])
-
 from itertools import chain
-from operator import mul
+from operator import mul, or_
 
-o = reduce(mul,sorted(chain(*[[len(recurse(f, x, y)) for y in range(len(f[x])) if any(f[x][y] < f[z][w] for z, w in POSITIONS(x, y) if z in range(len(f)) and w in range(len(f[x])))] for x in range(len(f))]))[-3:])
+f = [[*map(int, x)] for x in map(str.strip, open(0).read().strip().split('\n'))]
+
+o = (lambda q:reduce(mul,sorted(chain(*[[len((lambda r,*a:r(r,*a))(lambda r,f,x,y:{(x,y)}|reduce(or_,[({(x,y)}|r(r,f,z,w)if z in range(len(f))and w in range(len(f[x]))and f[x][y]<f[z][w]<9else set())for z,w in q(x,y)]),f,x,y))for y in range(len(f[x]))if any(f[x][y]<f[z][w]for z,w in q(x,y)if z in range(len(f))and w in range(len(f[x])))]for x in range(len(f))]))[-3:]))(lambda x,y:((x+1,y),(x-1,y),(x,y+1),(x,y-1)))
 
 print(o)
