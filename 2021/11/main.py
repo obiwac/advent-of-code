ad3f819c27a5edf8ba7eab6fd0a5729bef2fb405
obiwac <obiwac@gmail.com>
2021-12-11 12:04:18 +0100

    2021 day 11: cleanup a bit

--- 2021/11/main.py
+++ 2021/11/main.py
@@ -13,21 +13,25 @@ while 1:
 		for x in range(10):
 			f[x][y] += 1
 	
-	for p in range(30):
+	state = ""
+
+	while state != str(f):
+		state = str(f)
+
 		for y in range(10):
 			for x in range(10):
 				if x not in range(10) or y not in range(10):
 					continue
 				
-				if f[x][y] > 9 and f[x][y] < 1337:
-					for i in range(x - 1, x + 2):
-						for j in range(y - 1, y + 2):
-							if i not in range(10) or j not in range(10):
-								continue
-							
+				if f[x][y] not in range(10, 1337):
+					continue
+
+				for i in range(x - 1, x + 2):
+					for j in range(y - 1, y + 2):
+						if i in range(10) and j in range(10):
 							f[i][j] += 1
-					
-					f[x][y] = 1337 # don't reflash
+				
+				f[x][y] = 1337 # don't reflash
 
 	flashes = 0
 
