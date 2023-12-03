7119d2d011da255c8b5c7741f74b1d7097eab1b1
obiwac <obiwac@gmail.com>
2023-12-03 21:23:10 +0100

    2023/4: Part 2

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -1,5 +1,6 @@
 *f, = map(str.strip, open(0).readlines())
 s = 0
+card_counts = []
 
 for card in f:
 	_, numbers = card.split(":")
@@ -9,12 +10,23 @@ for card in f:
 	*got, = map(int, got.split())
 
 	a = 0
+	card_count = 0
 
 	for g in got:
 		if g in winning:
 			if not a: a = 1
 			else: a *= 2
 
+			card_count += 1
+
+	card_counts.append(card_count)
 	s += a
 
+instances = [1] * len(card_counts)
+
+for i, card_count in enumerate(card_counts):
+	for j in range(card_count):
+		instances[i + 1 + j] += 1 * instances[i]
+
 print(s)
+print(sum(instances))
