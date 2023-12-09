3ec78b51b4b1e078fb0843b465024f6d66596188
obiwac <obiwac@gmail.com>
2023-12-09 06:12:15 +0100

    2023/9: Part 2 - happy with this one, couldn't have been much faster I think

--- 2023/9/main.py
+++ 2023/9/main.py
@@ -25,3 +25,30 @@ for l in f:
 	s += chain[0][-1]
 
 print(s)
+
+# part 2
+
+s = 0
+
+for l in f:
+	*a, = map(int, l.split())
+	*a, = reversed(a) # this is all that changes!
+	chain = [a]
+
+	while a != [0] * len(a):
+		b = []
+		
+		for i in range(1, len(a)):
+			b.append(a[i] - a[i - 1])
+
+		chain.append(b)
+		a = b
+
+	chain[-1].append(0)
+
+	for c in range(len(chain) - 2, -1, -1):
+		chain[c].append(chain[c][-1] + chain[c + 1][-1])
+
+	s += chain[0][-1]
+
+print(s)
