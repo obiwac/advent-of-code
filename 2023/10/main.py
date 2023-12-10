23ef93a27de1aef54b4b57b44b3471eb0ddb8eda
obiwac <obiwac@gmail.com>
2023-12-10 06:54:22 +0100

    2023/10: Part 2 - I didn't understand part 2 initially, but leaving my impl here for posterity

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -53,3 +53,53 @@ while q:
 # 	print("".join(map(lambda x: str(x) if x >= 0 else ".", d)))
 
 print(max(sum(dists, [])) + 1)
+
+NONE = -1
+IN = 0
+OUT = 1
+
+status = [[NONE] * len(f[0]) for _ in range(len(f))]
+
+for y, row in enumerate(f):
+	for x, col in enumerate(row):
+		if col != ".":
+			continue
+
+		if status[y][x] != NONE:
+			continue
+
+		q = [(x, y)]
+		status[y][x] = IN
+		done = [(x, y)]
+		is_out = False
+
+		while q:
+			x, y = q.pop(0)
+
+			for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)):
+				nx = x + dx
+				ny = y + dy
+
+				if ny >= len(f) or ny < 0 or nx >= len(f[0]) or nx < 0:
+					is_out = True
+					continue
+
+				if f[ny][nx] != ".":
+					continue
+
+				if status[ny][nx] == NONE:
+					status[ny][nx] = IN
+					done.append((nx, ny))
+					q.append((nx, ny))
+
+		if is_out:
+			for x, y in done:
+				status[y][x] = OUT
+
+for y, row in enumerate(f):
+	for x, col in enumerate(row):
+		print(f[y][x] if status[y][x] == NONE else ("I" if status[y][x] == IN else "O"), end="")
+
+	print()
+
+print(sum(status, []).count(IN))
