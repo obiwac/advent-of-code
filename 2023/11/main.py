a38de0038a6d3f6152a92afe28eed5f4a8768688
obiwac <obiwac@gmail.com>
2023-12-11 06:51:00 +0100

    2023/11: Part 2 - analytical solution (which I failed with an off-by-one)

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,9 +1,15 @@
+from copy import deepcopy
+
 *f, = map(list, open(0).read().strip().split('\n'))
+nf = deepcopy(f) # no expansion
 
 of = f
 f = []
 
 for row in of:
+	if all(x == "." for x in row):
+		f.append(row)
+
 	f.append(row)
 
 *f, = zip(*f)
@@ -12,29 +18,41 @@ of = f
 f = []
 
 for row in of:
+	if all(x == "." for x in row):
+		f.append(row)
+
 	f.append(row)
 
 *f, = zip(*f)
 
-galaxies = {}
-counter = 1
+def dist_sum(f):
+	galaxies = {}
+	counter = 1
 
-for y, row in enumerate(f):
-	for x, col in enumerate(row):
-		if col != "#":
-			continue
+	for y, row in enumerate(f):
+		for x, col in enumerate(row):
+			if col != "#":
+				continue
 
-		galaxies[counter] = (x, y)
-		counter += 1
+			galaxies[counter] = (x, y)
+			counter += 1
 
-s = 0
+	s = 0
 
-for a in range(1, counter):
-	for b in range(a + 1, counter):
-		ax, ay = galaxies[a]
-		bx, by = galaxies[b]
+	for a in range(1, counter):
+		for b in range(a + 1, counter):
+			ax, ay = galaxies[a]
+			bx, by = galaxies[b]
 
-		dist = abs(bx - ax) + abs(by - ay)
-		s += dist
+			dist = abs(bx - ax) + abs(by - ay)
+			s += dist
 
-print(s)
+	return s
+
+no_expanded_dist = dist_sum(nf)
+expanded_dist = dist_sum(f)
+
+print(expanded_dist)
+
+delta = expanded_dist - no_expanded_dist
+print(no_expanded_dist + delta * 999999)
