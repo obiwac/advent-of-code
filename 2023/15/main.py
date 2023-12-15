91207ddb3185644c69f78622f0e3a2a67aa2d4b8
obiwac <obiwac@gmail.com>
2023-12-15 06:15:27 +0100

    2023/15: Part 2

--- 2023/15/main.py
+++ 2023/15/main.py
@@ -1,3 +1,5 @@
+from typing import OrderedDict
+
 *f, = map(str, open(0).read().strip().split('\n'))
 
 def hash(s):
@@ -12,3 +14,24 @@ def hash(s):
 
 ops = f[0].split(",")
 print(sum(map(hash, ops)))
+
+boxes = [OrderedDict() for _ in range(256)]
+
+for o in ops:
+	if "-" in o:
+		h = hash(o[:-1])
+		try: del boxes[h][o[:-1]]
+		except: pass
+	
+	elif "=" in o:
+		k, v = o.split("=")
+		h = hash(k)
+		boxes[h][k] = v
+
+s = 0
+
+for i, b in enumerate(boxes):
+	for j, k in enumerate(b):
+		s += (i + 1) * (j + 1) * int(b[k])
+
+print(s)
