bf8ca0ca284990d6fd677b30081ef5e5fa893ed9
obiwac <obiwac@gmail.com>
2023-12-20 11:48:42 +0100

    2023/20: Part 2 - much easier that I thought 😁

--- 2023/20/main.py
+++ 2023/20/main.py
@@ -1,74 +1,90 @@
+import math
+
 *f, = map(str, open(0).read().strip().split('\n'))
-d = {}
 
-for i in f:
-	in_, out = i.split(" -> ")
-	kind = "broadcaster"
-	
-	if in_[0] == "%":
-		in_ = in_[1:]
-		kind = "flipflop"
+for p2 in (False, True):
+	d = {}
 
-	if in_[0] == "&":
-		in_ = in_[1:]
-		kind = "conjunction"
+	for i in f:
+		in_, out = i.split(" -> ")
+		kind = "broadcaster"
 
-	# (kind, [ins], [out], flipflop_state, conjunction_mem)
-	d[in_] = [kind, [], out.split(", "), False, {}]
+		if in_[0] == "%":
+			in_ = in_[1:]
+			kind = "flipflop"
 
-to_add = []
+		if in_[0] == "&":
+			in_ = in_[1:]
+			kind = "conjunction"
 
-for k, v in d.items():
-	for o in v[2]:
-		if o not in d:
-			to_add.append(o)
-			continue
+		# (kind, [ins], [out], flipflop_state, conjunction_mem)
+		d[in_] = [kind, [], out.split(", "), False, {}]
 
-		if d[o][0] == "conjunction":
-			kind, ins, out, flipflop_state, mem = d[o]
-			ins.append(k)
-			mem[k] = False
+	to_add = []
 
-for i in to_add:
-	d[i] = ["idkfam", [], [], False, False]
-
-low_count = 0
-high_count = 0
-
-for _ in range(1000):
-	q = [("broadcaster", False, "button")]
-
-	while q:
-		name, is_high, prev = q.pop(0)
-		kind, ins, out, flipflop_state, mem = d[name]
-
-		is_high_name = "high" if is_high else "low"
-		print(f"{prev} -{is_high_name}-> {name}")
-
-		low_count += not is_high
-		high_count += is_high
-
-		if kind == "broadcaster":
-			for o in out:
-				if d[o][0] == "conjunction": d[o][4][name] = is_high
-				q.append((o, is_high, name))
-
-		if kind == "flipflop":
-			if is_high:
+	for k, v in d.items():
+		for o in v[2]:
+			if o not in d:
+				to_add.append(o)
 				continue
 
-			flipflop_state = not flipflop_state
-			d[name][3] = flipflop_state
+			if d[o][0] == "conjunction":
+				kind, ins, out, flipflop_state, mem = d[o]
+				ins.append(k)
+				mem[k] = False
 
-			for o in out:
-				if d[o][0] == "conjunction": d[o][4][name] = flipflop_state
-				q.append((o, flipflop_state, name))
+	for i in to_add:
+		d[i] = ["idkfam", [], [], False, False]
 
-		if kind == "conjunction":
-			s = sum(mem.values())
+	low_count = 0
+	high_count = 0
+	presses = 0
+	dep_presses = {}
 
-			for o in out:
-				if d[o][0] == "conjunction": d[o][4][name] = s != len(ins)
-				q.append((o, s != len(ins), name))
+	for _ in range(1000000000 if p2 else 1000):
+		q = [("broadcaster", False, "button")]
+		presses += 1
 
-print(low_count * high_count)
+		while q:
+			name, is_high, prev = q.pop(0)
+			kind, ins, out, flipflop_state, mem = d[name]
+
+			is_high_name = "high" if is_high else "low"
+			# print(f"{prev} -{is_high_name}-> {name}")
+
+			low_count += not is_high
+			high_count += is_high
+
+			if kind == "broadcaster":
+				for o in out:
+					if d[o][0] == "conjunction": d[o][4][name] = is_high
+					q.append((o, is_high, name))
+
+			if kind == "flipflop":
+				if is_high:
+					continue
+
+				flipflop_state = not flipflop_state
+				d[name][3] = flipflop_state
+
+				for o in out:
+					if d[o][0] == "conjunction": d[o][4][name] = flipflop_state
+					q.append((o, flipflop_state, name))
+
+			if kind == "conjunction":
+				s = sum(mem.values())
+
+				if p2 and name == "lb": # XXX a bit hardcoded whatever
+					for k, v in mem.items():
+						if v:
+							dep_presses[k] = presses
+
+					if len(dep_presses) == len(ins):
+						print(math.lcm(*dep_presses.values()))
+						exit()
+
+				for o in out:
+					if d[o][0] == "conjunction": d[o][4][name] = s != len(ins)
+					q.append((o, s != len(ins), name))
+
+	print(low_count * high_count)
