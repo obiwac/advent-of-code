33beeae34ec84883fa69e235ec2c11f497e2a4cb
obiwac <obiwac@gmail.com>
2023-12-25 07:57:31 +0100

    2022/24: Commit uncomitted changes
    Too lazy to clean things up :)

---
+++ 2022/24/main.py
@@ -0,0 +1,14 @@
+import math
+from itertools import chain
+from itertools import accumulate
+from functools import reduce
+from collections import Counter
+from collections import defaultdict
+from copy import deepcopy
+
+import numpy as np
+import heapq
+
+f = open(0).read().strip().split('\n')
+
+x, y 
