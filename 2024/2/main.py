c7ac2674e8f88624278df8a47de3b04eac9ea4b4
Aymeric Wibo <obiwac@gmail.com>
2024-12-02 06:11:03 +0100

    2024/2: Part 2
    Bit of an ugly bruteforce solution but eh.

--- 2024/2/main.py
+++ 2024/2/main.py
@@ -1,24 +1,44 @@
 f = open(0).readlines()
 
+def check_safety(x):
+	sorted_ = sorted(x)
+
+	if sorted_ != x and sorted_[::-1] != x:
+		return False
+
+	for i in range(len(x) - 1):
+		if not (1 <= abs(x[i + 1] - x[i]) <= 3):
+			return False
+
+	return True
+
 # Part 1.
 
 s = 0
 
 for l in f:
-	*x, = map(int, l.split())
-	sorted_ = sorted(x)
+	*y, = map(int, l.split())
+	s += check_safety(y)
 
-	if sorted_ != x and sorted_[::-1] != x:
+print(s)
+
+# Part 2.
+
+s = 0
+
+for l in f:
+	*y, = map(int, l.split())
+
+	if check_safety(y):
+		s += 1
 		continue
 
-	unsafe = False
+	for i in range(len(y)):
+		o = list(y)
+		del o[i]
 
-	for i in range(len(x) - 1):
-		if not (1 <= abs(x[i + 1] - x[i]) <= 3):
-			unsafe = True
+		if check_safety(o):
+			s += 1
 			break
-
-	if not unsafe:
-		s += 1
 
 print(s)
