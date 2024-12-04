b89fd049432452d826c5c858bd3334061e7a14c3
Aymeric Wibo <obiwac@gmail.com>
2024-12-04 07:02:14 +0100

    2024/4: Clean up

--- 2024/4/main.py
+++ 2024/4/main.py
@@ -1,14 +1,44 @@
 import re
 
+def flip(x):
+	return ["".join(reversed(l)) for l in x]
+
 *f, = map(str.strip, open(0).readlines())
+options = [f, flip(f)]
+
 *transposed, = map("".join, zip(*f))
+options += [transposed, flip(transposed)]
+
+for option in options[:2]:
+	diags = []
+
+	for i in range(-len(f), len(f)):
+		diag = ""
+
+		for j in range(len(f)):
+			if j + i < 0 or j + i >= len(f):
+				continue
+
+			diag += option[j + i][j]
+
+		diags.append(diag)
+
+	options.extend([diags, flip(diags)])
+
+# Part 1.
 
 s = 0
 
-options = [f, ["".join(reversed(l)) for l in f]]
-options += [transposed, ["".join(reversed(l)) for l in transposed]]
+for option in options:
+	s += sum(len(re.findall("XMAS", l)) for l in option)
 
-for oc, option in enumerate(options):
+print(s)
+
+# Part 2.
+
+s = 0
+
+for option in options[:4]:
 	for i, l in enumerate(option[:-2]):
 		for match in re.finditer("(?=(M.S))", l):
 			if option[i + 1][match.start() + 1] != "A":
@@ -21,49 +51,5 @@ for oc, option in enumerate(options):
 				continue
 
 			s += 1
-
-print(s)
-exit()
-
-for l in transposed:
-	s += len(re.findall("XMAS", l))
-	s += len(re.findall("SAMX", l))
-
-diag1 = []
-
-for i in range(-len(f), len(f)):
-	diag = ""
-
-	for j in range(len(f)):
-		if j + i < 0 or j + i >= len(f):
-			continue
-
-		diag += f[j + i][j]
-
-	diag1.append(diag)
-
-diag2 = []
-
-for i in range(-len(f), len(f)):
-	diag = ""
-
-	for j in range(len(f)):
-		if j + i < 0 or j + i >= len(f):
-			continue
-
-		diag += list(reversed(f[j + i].strip()))[j]
-
-	diag2.append(diag)
-
-print(diag1)
-print(diag2)
-
-for l in diag1:
-	s += len(re.findall("XMAS", l))
-	s += len(re.findall("SAMX", l))
-
-for l in diag2:
-	s += len(re.findall("XMAS", l))
-	s += len(re.findall("SAMX", l))
 
 print(s)
