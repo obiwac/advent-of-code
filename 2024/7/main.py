e273286a2cfcf380402586f43e609dcb9533e2c0
Aymeric Wibo <obiwac@gmail.com>
2024-12-07 07:09:12 +0200

    2024/7: Part 2

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -1,7 +1,9 @@
 f = open(0).read().strip().split("\n")
 ts = 0
 
-for l in f:
+for z, l in enumerate(f):
+	print(f"{z}/{len(f)}")
+
 	lv, rv = l.split(":")
 	lv = int(lv)
 	*a, = map(int, rv.strip().split())
@@ -9,15 +11,22 @@ for l in f:
 	works = False
 
 	for i in range(3 ** (len(a) - 1)):
-		ops = bin(i)[2:].zfill(len(a) - 1).replace("1", "*").replace("0", "+")
+		ops = [0] * (len(a) - 1)
+
+		for j in range(len(a) - 1):
+			ops[j] = i // (3 ** j) % 3
+
 		s = a[0]
 
 		for i, o in enumerate(ops):
-			if o == "+":
+			if o == 0:
 				s += a[i + 1]
 
-			elif o == "*":
+			elif o == 1:
 				s *= a[i + 1]
+
+			elif o == 2:
+				s = int(str(s) + str(a[i + 1]))
 
 		works |= s == lv
 
