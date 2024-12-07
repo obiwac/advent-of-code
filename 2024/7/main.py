5aa39056582ba8dd4857ac93dc8d214859321b8c
Aymeric Wibo <obiwac@gmail.com>
2024-12-07 07:10:26 +0200

    2024/7: Combine parts

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -1,36 +1,38 @@
 f = open(0).read().strip().split("\n")
-ts = 0
 
-for z, l in enumerate(f):
-	print(f"{z}/{len(f)}")
+for base in (2, 3):
+	ts = 0
 
-	lv, rv = l.split(":")
-	lv = int(lv)
-	*a, = map(int, rv.strip().split())
+	for z, l in enumerate(f):
+		# print(f"{z}/{len(f)}")
 
-	works = False
+		lv, rv = l.split(":")
+		lv = int(lv)
+		*a, = map(int, rv.strip().split())
 
-	for i in range(3 ** (len(a) - 1)):
-		ops = [0] * (len(a) - 1)
+		works = False
 
-		for j in range(len(a) - 1):
-			ops[j] = i // (3 ** j) % 3
+		for i in range(3 ** (len(a) - 1)):
+			ops = [0] * (len(a) - 1)
 
-		s = a[0]
+			for j in range(len(a) - 1):
+				ops[j] = i // (base ** j) % base
 
-		for i, o in enumerate(ops):
-			if o == 0:
-				s += a[i + 1]
+			s = a[0]
 
-			elif o == 1:
-				s *= a[i + 1]
+			for i, o in enumerate(ops):
+				if o == 0:
+					s += a[i + 1]
 
-			elif o == 2:
-				s = int(str(s) + str(a[i + 1]))
+				elif o == 1:
+					s *= a[i + 1]
 
-		works |= s == lv
+				elif o == 2:
+					s = int(str(s) + str(a[i + 1]))
 
-	if works:
-		ts += lv
+			works |= s == lv
 
-print(ts)
+		if works:
+			ts += lv
+
+	print(ts)
