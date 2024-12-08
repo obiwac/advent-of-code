c6ce2711486d0c312dbc6263e2d9496cc4ade5f9
Aymeric Wibo <obiwac@gmail.com>
2024-12-08 07:20:36 +0200

    2024/8: Part 2

--- 2024/8/main.py
+++ 2024/8/main.py
@@ -23,19 +23,43 @@ for freq in freqs:
 
 			def try_add(i, j):
 				if i < 0 or i >= len(f) or j < 0 or j >= len(f[i]):
-					return
+					return 1
 
 				antinodes.add((i, j))
+				return 0
 
-			try_add(pos1[0] + dy, pos1[1] + dx)
-			try_add(pos2[0] - dy, pos2[1] - dx)
+			acc_y = pos1[0]
+			acc_x = pos1[1]
+
+			while 1:
+				if try_add(acc_y, acc_x):
+					break
+
+				acc_y += dy
+				acc_x += dx
+
+			acc_y = pos2[0]
+			acc_x = pos2[1]
+
+			while 1:
+				if try_add(acc_y, acc_x):
+					break
+
+				acc_y -= dy
+				acc_x -= dx
+	
+done = 0
 
 for i in range(len(f)):
 	for j in range(len(f[i])):
+		#if f[i][j] != ".":
+		#	print(f[i][j], end="")
 		if (i, j) in antinodes:
+			done += 1
 			print("X", end="")
 		else:
 			print(f[i][j], end="")
 	print()
 
 print(len(antinodes))
+print(done)
