1fcfa8c3cced6e465fa30af0057b086d0d9b35de
Aymeric Wibo <obiwac@gmail.com>
2024-12-10 06:16:22 +0100

    2024/10: Part 2

--- 2024/10/main.py
+++ 2024/10/main.py
@@ -6,24 +6,24 @@ for l in f:
 
 def dfs(i, j, prev, path):
 	if i < 0 or i >= len(m) or j < 0 or j >= len(m[i]):
-		return set()
+		return 0
 
 	if m[i][j] != prev + 1:
-		return set()
+		return 0
 
 	if (i, j) in path:
-		return set()
+		return 0
 
 	if m[i][j] == 9:
-		return {(i, j)}
+		return 1
 
-	nines = set()
+	nines = 0
 	npath = path + [(i, j)]
 
-	nines |= dfs(i - 1, j, m[i][j], npath)
-	nines |= dfs(i + 1, j, m[i][j], npath)
-	nines |= dfs(i, j - 1, m[i][j], npath)
-	nines |= dfs(i, j + 1, m[i][j], npath)
+	nines += dfs(i - 1, j, m[i][j], npath)
+	nines += dfs(i + 1, j, m[i][j], npath)
+	nines += dfs(i, j - 1, m[i][j], npath)
+	nines += dfs(i, j + 1, m[i][j], npath)
 
 	return nines
 
@@ -33,6 +33,6 @@ for i in range(len(m)):
 	for j in range(len(m[i])):
 		if m[i][j] == 0:
 			score = dfs(i, j, -1, [])
-			s += len(score)
+			s += score
 
 print(s)
