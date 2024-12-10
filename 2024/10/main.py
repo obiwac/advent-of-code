47b07422477d931dbbe83d180b627756ee4d00cc
Aymeric Wibo <obiwac@gmail.com>
2024-12-10 06:19:54 +0100

    2024/10: Both parts

--- 2024/10/main.py
+++ 2024/10/main.py
@@ -6,33 +6,37 @@ for l in f:
 
 def dfs(i, j, prev, path):
 	if i < 0 or i >= len(m) or j < 0 or j >= len(m[i]):
-		return 0
+		return set(), 0
 
 	if m[i][j] != prev + 1:
-		return 0
+		return set(), 0
 
 	if (i, j) in path:
-		return 0
+		return set(), 0
 
 	if m[i][j] == 9:
-		return 1
+		return {(i, j)}, 1
 
-	nines = 0
+	nines = set()
+	tot = 0
 	npath = path + [(i, j)]
 
-	nines += dfs(i - 1, j, m[i][j], npath)
-	nines += dfs(i + 1, j, m[i][j], npath)
-	nines += dfs(i, j - 1, m[i][j], npath)
-	nines += dfs(i, j + 1, m[i][j], npath)
+	nines, tot = (lambda x: (nines | x[0], tot + x[1]))(dfs(i - 1, j, m[i][j], npath))
+	nines, tot = (lambda x: (nines | x[0], tot + x[1]))(dfs(i + 1, j, m[i][j], npath))
+	nines, tot = (lambda x: (nines | x[0], tot + x[1]))(dfs(i, j - 1, m[i][j], npath))
+	nines, tot = (lambda x: (nines | x[0], tot + x[1]))(dfs(i, j + 1, m[i][j], npath))
 
-	return nines
+	return nines, tot
 
-s = 0
+p1 = 0
+p2 = 0
 
 for i in range(len(m)):
 	for j in range(len(m[i])):
 		if m[i][j] == 0:
-			score = dfs(i, j, -1, [])
-			s += score
+			score, rating = dfs(i, j, -1, [])
+			p1 += len(score)
+			p2 += rating
 
-print(s)
+print(p1)
+print(p2)
