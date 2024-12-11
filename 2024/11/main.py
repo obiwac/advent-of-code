b9db1788bbfdc6f3da70fe39159d1314b085b8aa
Aymeric Wibo <obiwac@gmail.com>
2024-12-11 06:14:11 +0100

    2024/11: Part 2

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -1,20 +1,22 @@
+from functools import cache
+
 f = open(0).read().strip().split("\n")
 *a, = map(int, f[0].split())
 
-for _ in range(25):
-	na = []
+@cache
+def g(x, i = 0):
+	if i == 25:
+		return 1
 
-	for i, x in enumerate(a):
-		if x == 0:
-			na.append(1)
+	if x == 0:
+		return g(1, i + 1)
 
-		elif len(str(x)) % 2 == 0:
-			na.append(int(str(x)[:len(str(x)) // 2]))
-			na.append(int(str(x)[len(str(x)) // 2:]))
+	elif len(str(x)) % 2 == 0:
+		l = int(str(x)[:len(str(x)) // 2])
+		r = int(str(x)[len(str(x)) // 2:])
 
-		else:
-			na.append(x * 2024)
+		return g(l, i + 1) + g(r, i + 1)
 
-	a = na
+	return g(x * 2024, i + 1)
 
-print(len(a))
+print(sum(g(x) for x in a))
