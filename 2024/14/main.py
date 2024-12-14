e030c26d1f05bfe8c585d80b2b0265e8cd5e5c65
Aymeric Wibo <obiwac@gmail.com>
2024-12-14 06:47:51 +0100

    2024/14: Both parts

--- 2024/14/main.py
+++ 2024/14/main.py
@@ -2,7 +2,7 @@ W = 101
 H = 103
 ITER = 100
 
-f = open("input").read().strip().split("\n")
+f = open(0).read().strip().split("\n")
 q = [0, 0, 0, 0]
 r = []
 
@@ -16,8 +16,38 @@ for l in f:
 	vy = int(vy)
 	r.append((px, py, vx, vy))
 
-map = [[0 for i in range(W)] for j in range(H)]
+# Part 1.
+
+for l in f:
+	p, v = l.split()
+	px, py = p.split(",")
+	vx, vy = v.split(",")
+	px = int(px[2:])
+	vx = int(vx[2:])
+	py = int(py)
+	vy = int(vy)
+
+	x = (px + vx * ITER) % W
+	y = (py + vy * ITER) % H
+
+	if x < W // 2 and y < H // 2:
+		q[0] += 1
+
+	elif x > W // 2 and y < H // 2:
+		q[1] += 1
+
+	elif x < W // 2 and y > H // 2:
+		q[2] += 1
+
+	elif x > W // 2 and y > H // 2:
+		q[3] += 1
+
+print(q[0] * q[1] * q[2] * q[3])
+
+# Part 2.
+
 c = []
+k = 0
 
 for i, (px, py, _, _) in enumerate(r):
 	c.append([px, py])
@@ -32,7 +62,6 @@ for k in range(1, 1000000):
 		seen.add((c[i][0], c[i][1]))
 
 	if len(seen) == len(f):
-		print("Found in ", k)
 		break
 
 for i in range(H):
@@ -41,64 +70,10 @@ for i in range(H):
 			if x == j and y == i:
 				print("#", end="")
 				break
+
 		else:
 			print(".", end="")
+
 	print()
 
-exit()
-
-for k in range(1000000):
-	#map = [[0 for i in range(W)] for j in range(H)]
-
-	for px, py, vx, vy in r:
-		x = (px + vx * ITER) % W
-		y = (py + vy * ITER) % H
-
-		cx = px
-		cy = py
-
-		seen = set()
-
-		for _ in range(k):
-			#if map[cy][cx] > 0:
-			#	map[cy][cx] -= 1
-
-			cx = (cx + vx) % W
-			cy = (cx + vy) % H
-
-			seen.add((cx, cy))
-
-			#map[cy][cx] += 1
-
-		if len(seen) == len(f):
-			print("Found in ", k)
-			break
-
-		if x < W // 2 and y < H // 2:
-			q[0] += 1
-
-		elif x > W // 2 and y < H // 2:
-			q[1] += 1
-		
-		elif x < W // 2 and y > H // 2:
-			q[2] += 1
-		
-		elif x > W // 2 and y > H // 2:
-			q[3] += 1
-
-		# if map in maps:
-		# 	print("Loop in ", k)
-		# 	break
-
-		# maps.append(map)
-
-		# for i in range(H):
-		# 	for j in range(W):
-		# 		if map[i][j] == 0:
-		# 			print(" ", end="")
-		# 		else:
-		# 			print(map[i][j], end="")
-		# 	print()
-
-print(q)
-print(q[0] * q[1] * q[2] * q[3])
+print(k)
