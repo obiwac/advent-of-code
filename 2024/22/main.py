5c054005f4ed3cab267d9cb39ad0ccbfc832b71d
Aymeric Wibo <obiwac@gmail.com>
2024-12-22 11:36:48 +0100

    2024/22: Both parts

--- 2024/22/main.py
+++ 2024/22/main.py
@@ -1,14 +1,38 @@
 f = open(0).read().strip().split("\n")
 s = 0
+totals = {}
 
 for l in f:
 	x = int(l)
+	changes = []
+	visited = set()
 
-	for i in range(2000):
-		x = (x ^ (x << 6)) % 16777216
-		x = (x ^ (x >> 5)) % 16777216
-		x = (x ^ (x << 11)) % 16777216
+	for _ in range(2000):
+		prev = x % 10
+		x = (x ^ (x << 6)) & 0xFFFFFF
+		x = (x ^ (x >> 5)) & 0xFFFFFF
+		x = (x ^ (x << 11)) & 0xFFFFFF
+
+		price = x % 10
+		changes.append(price - prev)
+
+		if len(changes) < 4:
+			continue
+
+		k = tuple(changes[-4:])
+
+		if k in visited:
+			continue
+
+		visited.add(k)
+
+		if k not in totals:
+			totals[k] = price
+
+		else:
+			totals[k] += price
 
 	s += x
 
 print(s)
+print(max(totals.values()))
