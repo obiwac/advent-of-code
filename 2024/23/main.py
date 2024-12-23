c849c443091b736183b7a7634f32e2e352d3391a
Aymeric Wibo <obiwac@gmail.com>
2024-12-23 07:32:35 +0100

    2024/23: Part 2

--- 2024/23/main.py
+++ 2024/23/main.py
@@ -21,3 +21,31 @@ for u, v in edges:
 				triples.add(frozenset((u, v, w)))
 
 print(len(triples))
+
+A = {}
+
+for l in f:
+	a, b = l.split("-")
+
+	if a not in A:
+		A[a] = set()
+
+	if b not in A:
+		A[b] = set()
+
+	A[a].add(b)
+	A[b].add(a)
+
+cliques = set()
+
+def bron_kerbosch(R, P, X): # From Wikipedia.
+	if not P and not X:
+		cliques.add(frozenset(R))
+
+	for v in set(P):
+		bron_kerbosch(R | {v}, P & A[v], X & A[v])
+		P.remove(v)
+		X.add(v)
+
+bron_kerbosch(set(), set(A.keys()), set())
+print(",".join(sorted(list(max(cliques, key=len)))))
